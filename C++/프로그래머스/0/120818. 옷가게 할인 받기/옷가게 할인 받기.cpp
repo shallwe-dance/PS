064cc545642e42fbbf5151887ee129e7bc2fa18a
JEONG SEWOONG <154857779+shallwe-dance@users.noreply.github.com>
2026-07-01 17:48:15 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 4.71 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if (price>=500000) {return price*0.8;}
+    else if (price>=300000) {return price*0.9;}
+    else if (price>=100000) {return price*0.95;}
+    else {return price;}
+    return answer;
+}
