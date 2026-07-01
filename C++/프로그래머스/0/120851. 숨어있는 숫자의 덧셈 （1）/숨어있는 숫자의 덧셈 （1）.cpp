e265ef036f9ce84456edbbb60f2806fb28b686b4
JEONG SEWOONG <154857779+shallwe-dance@users.noreply.github.com>
2026-07-01 19:27:07 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.01 ms, Memory: 4.83 MB -BaekjoonHub

---
+++ C++/프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string) {
+    int answer = 0;
+    for (int i=0;i<my_string.length();i++) {
+        if (!isalpha(my_string[i])) {answer+=my_string[i]-'0';}
+    }
+    return answer;
+}
